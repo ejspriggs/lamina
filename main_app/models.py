434568541966360f5f8ee0c8770676740a7b264e
Mour Wagner <mourwagner13@gmail.com>
2024-02-04 21:20:53 -0500

    created an app for the project, outlined the animals and fossil models, and added some addtional files for views in the future

---
+++ main_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Animal(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=60)
+    image = models.CharField(max_length=256)
+    time_of_year = models.CharField(max_length=100)
+    south_hemi_toy = models.CharField(max_length=100)
+    time_of_day = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    shadow_size = models.CharField(max_length=100)
+    weather = models.CharField(max_length=100)
+    value = models.IntegerField()
+    misc = models.TextField(max_length=500)
+
+class Fossils(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=256)
+    value = models.IntegerField()
