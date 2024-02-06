399ee7ddedfe8f81942ea739a37b09881693e533
Mour Wagner <mourwagner13@gmail.com>
2024-02-05 22:24:51 -0500

    Made changes to the animal and fossils models to include a user as a foreign key

--- main_app/models.py
+++ main_app/models.py
@@ -1,26 +1,16 @@
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Animal(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=60)
-    image = models.CharField(max_length=256)
-    time_of_year = models.CharField(max_length=100)
-    south_hemi_toy = models.CharField(max_length=100)
-    time_of_day = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    shadow_size = models.CharField(max_length=100)
     weather = models.CharField(max_length=100)
-    value = models.IntegerField()
-    misc = models.TextField(max_length=500)
 
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Fossils(models.Model):
     name = models.CharField(max_length=100)
-    image = models.CharField(max_length=256)
-    value = models.IntegerField()
 
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
