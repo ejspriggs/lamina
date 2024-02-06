d4e3e09e727a456f28e59312fa5b06e4fbdf5607
Edward Spriggs <ejspriggs@gmail.com>
2024-02-06 10:35:12 -0500

    Added "Profile" model linked to User with name and is_northern_hemi fields, set up to be editable in admin panel.  Migrations look good.

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 
 class Animal(models.Model):
     name = models.CharField(max_length=100)
@@ -10,7 +9,14 @@ class Animal(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+
 class Fossils(models.Model):
     name = models.CharField(max_length=100)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    display_name = models.CharField(max_length = 100)
+    is_northern_hemi = models.BooleanField()
