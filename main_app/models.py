00d80833c922ec653f5482e7125a2a6f1fe48df9
Mour Wagner <mourwagner13@gmail.com>
2024-02-08 11:06:52 -0500

    Fixed configuration page to only show items that are currently in the database

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 
@@ -9,12 +10,13 @@ class Animal(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-
 class Fossils(models.Model):
     name = models.CharField(max_length=100)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def get_absolute_url(self):
+        return reverse('fossils_index')
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
