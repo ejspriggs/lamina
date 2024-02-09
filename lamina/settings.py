6ce2fa1d4d87f9cf8be02fd6673bc07c626eba52
Edward Spriggs <ejspriggs@gmail.com>
2024-02-09 17:06:17 -0500

    No longer needed now that django_on_heroku is included in settings.py

--- lamina/settings.py
+++ lamina/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ['MODE'] == 'dev' else False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'lamina-49b984304f57.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
